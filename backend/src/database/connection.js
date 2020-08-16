c9b8b963df0e88b95d9b578fa6b7a3747e3d922e
jorgsiq <jorge.siqueira@yandex.com>
2020-08-15 22:40:01 -0300

    mobile commit

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,5 +1,10 @@
-const knex = require("knex");
-const configuration = require("../../knexfile");
+const knex = require('knex');
+const configuration = require('../../knexfile');
+
+const env =
+	process.env.NODE_ENV === 'test'
+		? configuration.test
+		: configuration.development;
 
 const connection = knex(configuration.development);
 
